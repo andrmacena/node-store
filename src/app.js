87b99ebe85b0dd41533628e78b361e45c163b3b5
Andre Macena <andrmacena_@outlook.com>
2020-01-06 17:07:05 -0300

    adicionado key para envio de email e storage

--- src/app.js
+++ src/app.js
@@ -15,8 +15,19 @@ const productRoutes = require('./routes/product')
 const customerRoutes = require('./routes/customer')
 const orderRoutes = require('./routes/order')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({
+    limit: '5mb'
+}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next()
+})
 
 app.use('/', indexRoute)
 app.use('/products', productRoutes)
