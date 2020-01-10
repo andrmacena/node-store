0e10738b6122c870821a4f9f01ab1494f38067e5
Andre Macena <andrmacena_@outlook.com>
2020-01-10 19:27:55 -0300

    Retirado sendgrid key

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store'
 
 module.exports = {
     connectionString: 'mongodb+srv://semana:rocketseat@cluster0-ocqjp.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey:'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',
+    sendgridKey:'<YOUR SENDGRID KEY HERE>',
     containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=storenode;AccountKey=IKsCFlPWyN3jV5f47QCw/3ZI5096lAdThF56/gzuD6Pgs193yitTecGmHF5CRuj+wZHZwceBtT1A/cdA2PTf+w==;EndpointSuffix=core.windows.net'
 }
