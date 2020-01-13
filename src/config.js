4f13a673725948e7ed62b97b42fcf6312028db19
Andre Macena <andrmacena_@outlook.com>
2020-01-13 09:03:23 -0300

    Removido chaves expostas

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'c07df704-fcae-4edf-9fc4-c2a8f61a03e3'
+global.SALT_KEY = '<YOUR SALT KEY HERE>'
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> seja bem vindo à Node Store'
 
 module.exports = {
     connectionString: 'mongodb+srv://semana:rocketseat@cluster0-ocqjp.mongodb.net/test?retryWrites=true&w=majority',
     sendgridKey:'<YOUR SENDGRID KEY HERE>',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=storenode;AccountKey=IKsCFlPWyN3jV5f47QCw/3ZI5096lAdThF56/gzuD6Pgs193yitTecGmHF5CRuj+wZHZwceBtT1A/cdA2PTf+w==;EndpointSuffix=core.windows.net'
+    containerConnectionString: '<YOUR AZURE STORAGE KEY HERE>'
 }
